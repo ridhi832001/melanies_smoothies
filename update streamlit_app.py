616719164e945228aaf0563f01bb322dc5d582a9
ridhi832001 <Ridhi.Sood@donaldson.com>
2025-12-02 19:09:52 +0530

    update streamlit_app.py

--- update streamlit_app.py
+++ update streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col ,add when_matched
+from snowflake.snowpark.functions import col 
 
 st.title("Customize your smoothie")
 
